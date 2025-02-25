63d85775ad9b78967ea9b59e3f30626bec42c24e
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2025-02-25 09:44:03 +0100

    Create BaseInput component

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,15 +1,24 @@
 <template>
   <p class="text-3xl font-bold underline">{{ count.toFixed(2) }}</p>
-
+  <base-input @update:model-value="handleModelUpdate" :model-value="name"/>
   <button @click="increment">click me</button>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
+import type { Ref } from 'vue'
+import BaseInput from '@/components/BaseInput.vue';
+
 function increment() {
   count.value++
 }
-let count = ref(1)
+
+function handleModelUpdate(val: string) {  
+  console.log('name', name.value, 'val', val);
+  name.value = val
+}
+let count: Ref<number> = ref(1)
+let name: Ref<string> = ref('')
 </script>
 
 <style lang="scss" scoped>
---
+++ src/components/BaseInput.vue
@@ -0,0 +1,35 @@
+<template>
+    <input 
+        @input="handleInput"
+        v-bind="$attrs"
+        :value="modelValue"
+    />
+</template>
+
+<script setup lang="ts">
+    const props = defineProps({
+        modelValue: {
+            type: String,
+            required: true
+        }
+    })
+
+    const emit = defineEmits([ 'update:modelValue' ])
+
+    defineOptions({
+        inheritAttrs: false
+    })
+
+    function handleInput(event: Event) {    
+        if (event.target) {   
+            emit('update:modelValue', (event.target as HTMLInputElement).value)
+        }
+        
+    }
+
+
+</script>
+
+<style scoped>
+
+</style>
